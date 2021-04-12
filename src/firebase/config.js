2baf41c44157265f49392ecaf4bf108ca3d68403
jeel-a-simformsolutions <jeel.a@simformsolutions.com>
2021-04-12 16:20:49 +0530

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// firebase config
+export const firebaseConfig = {
+    apiKey: "AIzaSyAuQ6S7t1E8HUcL81lNiQaNwI3_9HA6SwY",
+    authDomain: "oval-bricolage-306607.firebaseapp.com",
+    projectId: "oval-bricolage-306607",
+    storageBucket: "oval-bricolage-306607.appspot.com",
+    messagingSenderId: "877710601676",
+    appId: "1:877710601676:web:f0e72224162c5a6d8c8169",
+    measurementId: "G-ZTCN5681L6"
+};
